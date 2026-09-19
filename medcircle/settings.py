d7c36a273d5d3fdfaf2c22943a6085af0c69e4b7
yamini12112007 <yamini.s812335@gmail.com>
2026-09-19 13:12:43 +0530

    Modify STATIC_URL and add STATIC_ROOT
    Updated static file settings for Django project.

--- medcircle/settings.py
+++ medcircle/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
