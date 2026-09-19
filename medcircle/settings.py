7a05b5e2d154fd17e1379896412273b6bae42ebd
yamini12112007 <yamini.s812335@gmail.com>
2026-09-19 13:23:26 +0530

    Fix duplicate STATIC_ROOT definition in settings.py

--- medcircle/settings.py
+++ medcircle/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 
 # Email
