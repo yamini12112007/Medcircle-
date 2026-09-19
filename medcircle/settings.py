c9e612be146b028b2daf24166918f014a2a3bfb0
yamini12112007 <yamini.s812335@gmail.com>
2026-09-19 13:26:57 +0530

    Configure storage backends for static files
    Added storage configurations for static files.

--- medcircle/settings.py
+++ medcircle/settings.py
@@ -126,6 +126,14 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 # Email
