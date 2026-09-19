9dfae6b8d9ff442736486d0e6909cefd27feb098
yamini12112007 <yamini.s812335@gmail.com>
2026-09-19 13:09:25 +0530

    Update ALLOWED_HOSTS for deployment

--- medcircle/settings.py
+++ medcircle/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-h*=fn#4ksfw8!g&=!3++drwfzl-0%*hn!)xaz+gkav%8u+qp9l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["medcircle-1.onrender.com",
+    "localhost",
+    "127.0.0.1",]
 
 
 # Application definition
